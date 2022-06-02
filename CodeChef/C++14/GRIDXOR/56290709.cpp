ae0a125ef8c679b8ccac2f6c66481de57a19a2d4
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:04:46 +0530

    Committing files

---
+++ CodeChef/C++14/GRIDXOR/56290709.cpp
@@ -0,0 +1,59 @@
+/*I_Am_A_Noob*/
+
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<list>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<bitset>
+#include<iomanip>
+#include<numeric>
+#include<climits>
+
+#define ll long long
+#define MOD 1000000007
+#define _z ios_base ::sync_with_stdio(false);cin.tie(nullptr)
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+//#define for(i,n) for(int i=0;i<n;i++)
+#define pb emplace_back
+#define ppb pop_back
+#define endl "\n"
+#define int long long int
+int N=1000;
+int ans,temp,sum;
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    int arr[100][100]={1};
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            arr[i][j]=1;
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }   
+cout<<endl;
+}
+
+int32_t main()
+{
+    _z;
+    int t=1;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
