ace13bf3c380e4446983f141e8fb088b5f65266b
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:14:13 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1199B/142825015.cpp
@@ -0,0 +1,53 @@
+/*I_Am_A_Noob*/
+
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<list>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<bitset>
+#include<iomanip>
+#include<numeric>
+#include<climits>
+
+#define ll long long
+#define MOD 1000000007
+#define _z ios_base ::sync_with_stdio(false);cin.tie(nullptr)
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+//#define for(z,x,y) for(int i=z;i<x;i+=y)
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+#define endl "\n"
+#define int long long int
+
+int ans,temp,sum;
+using namespace std;
+
+void solve(){
+    double h,l;
+    cin>>h>>l;
+    cout<<fixed<<setprecision(10)<<((l*l)-(h*h))/(2*h);
+cout<<endl;
+}
+
+int32_t main()
+{
+    _z;
+    int t=1;
+    // cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
