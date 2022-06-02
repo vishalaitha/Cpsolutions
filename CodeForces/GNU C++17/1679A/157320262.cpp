41184574925499cb688c5bf7a97ba5c6a0b629e8
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:04:38 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1679A/157320262.cpp
@@ -0,0 +1,87 @@
+/*---------------------------------------------------------------------------------------------------------------------------*/
+// Author : I_Am_A_Noob
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#include<bits/stdc++.h>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//constants and typedefs
+#define int long long int
+#define MOD 1000000007
+#define epsilon = 0.0000001f
+#define PI 3.141592653589793238462
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//operations on containers
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//general operations
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define debug(x) cout << #x << ": " << x << endl
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//templates
+//template <class T> void _print(vector <T> v);
+//template <class T> void _print(set <T> v);
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define si set<int>
+#define mpii map<int, int>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+// ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int ans,temp,sum;
+using namespace std;
+void _print(long long t) {cout << t;}
+template <class T> void _print(vector <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+void solve(){
+    int n;
+    cin>>n;
+    if(n<=3||n%2==1){
+    	cout<<-1<<endl;
+    	return;
+    }
+    if(n==4){
+    	cout<<1<<" "<<1<<endl;
+    	return;
+    }
+    int mx,mn;
+    int fours=n/4,sixes=0;
+    int rem=n%4;
+    if(rem==2){
+    	fours--;
+    	sixes++;
+    }
+    mx=fours+sixes;
+    fours=0,sixes=n/6;
+    rem=n%6;
+    if(rem==2||rem==4){
+    	fours+=2;
+    	if(sixes>0)
+    		sixes--;
+    }
+    mn=fours+sixes;
+    cout<<mn<<" "<<mx<<endl;
+}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r" --------------------------------------------
+freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w" ----------------------------------------
+#endif
+    fastio();
+    int t=1;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
+/*---------------------------------------------------------------------------------------------------------------------------*/
