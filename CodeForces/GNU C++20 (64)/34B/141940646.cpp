307abf78987663c44c570e4db3118628fe1bca04
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:15:28 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/34B/141940646.cpp
@@ -0,0 +1,58 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<list>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<bitset>
+#include<iomanip>
+#include<numeric>
+#include<climits>
+#define ll long long
+#define MOD 1000000007
+#define _z ios_base ::sync_with_stdio(false);cin.tie(nullptr)
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+//#define for(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define endl "\n"
+#define int long long int
+int ans,temp,sum;
+using namespace std;
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    vi v;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        v.pb(temp);
+    }
+    sort(v.begin(),v.end());
+    int sum=0;
+    for(int i=0;i<m;i++){
+        if(v[i]<0){
+            sum+=abs(v[i]);
+        }
+    }
+    cout<<sum;
+cout<<endl;
+}
+int32_t main()
+{
+    _z;
+    int t=1;
+    // cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
