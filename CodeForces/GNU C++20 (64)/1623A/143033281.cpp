c9e71649b71ed89e06fdbc7a0c71c0171f8bef94
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:13:57 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1623A/143033281.cpp
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/*I_Am_A_Noob*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include <array>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<math.h>
+#include<bitset>
+#include<iomanip>
+#include<numeric>
+#include<climits>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define ll long long
+#define MOD 1000000007
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//#define for(z,x,y) for(int i=z;i<x;i+=y)
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+#define endl "\n"
+#define int long long int
+#define all(x) (x).begin(), (x).end()
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define vi vector<int>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define si set<int>
+#define mii map<int, int>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+void swap(char &x,char&y) {char temp= x; x=y; y=temp;}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int ans,temp,sum;
+using namespace std;
+
+void solve(){
+    int r,c,x1,y1,x2,y2;
+    cin>>r>>c>>x1>>y1>>x2>>y2;
+    int dr=1;
+    int dc=1;
+    int ans=0;
+    while(1){
+        if(x1==x2||y1==y2){
+            break;
+        }
+        if(x1+dr>r)
+            dr=-1;
+        // else if(x1+dr<1)
+        //     dr=1;
+        if(y1+dc>c)
+            dc=-1;
+        // else if(y1+dc<1)
+        //     dc=1;
+        x1+=dr;
+        y1+=dc;
+        ans++;
+    }
+    cout<<ans;
+cout<<endl;
+}
+
+int32_t main()
+{
+    fastio();
+    int t=1;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
