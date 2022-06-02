57a213e38ade90e4f561ff17c5bef33c8a31193d
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:04:36 +0530

    Committing files

---
+++ CodeChef/C++14/GFTSHP/60572534.cpp
@@ -0,0 +1,96 @@
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/*I_Am_A_Noob*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include <array>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<list>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<math.h>
+#include<bitset>
+#include<iomanip>
+#include<numeric>
+#include<climits>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define ll long long
+#define MOD 1000000007
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//#define for(z,x,y) for(int i=z;i<x;i+=y)
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+#define endl "\n"
+#define int long long int
+#define all(x) (x).begin(), (x).end()
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define vi vector<int>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define si set<int>
+#define mii map<int, int>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+void swap(char &x,char&y) {char temp= x; x=y; y=temp;}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int ans,temp,sum;
+using namespace std;
+
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    vi v;
+    // int maxi=
+    int bfsum=0;
+    for(int i=0;i<n;i++){
+        cin>>temp;
+        v.pb(temp);
+        bfsum+=v[i];
+    }
+    if(k>=bfsum){
+        cout<<n<<endl;
+        return;
+    }
+    sort(v.begin(),v.end());
+    bool firstitem=1;
+    int count=0,sum=0;
+    for(int i=0;i<n;i++){
+        if(sum+v[i]<=k){
+            count++;
+            sum+=v[i];
+        }
+        else if(sum+((v[i]+1)/2)<=k&&firstitem){
+            count++;
+            sum+=ceil((v[i]+1)/2);
+            firstitem=0;
+        }
+    }
+    cout<<count<<endl;
+// cout<<endl;
+}
+
+int32_t main()
+{
+    fastio();
+    int t=1;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
+
