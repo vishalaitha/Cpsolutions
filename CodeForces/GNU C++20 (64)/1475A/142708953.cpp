2a74685d808bdc1fa7a46bf9f1f10b3cbf49e3bc
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:14:24 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1475A/142708953.cpp
@@ -0,0 +1,57 @@
+/*I_Am_A_Noob*/
+
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<vector>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<list>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<bitset>
+#include<iomanip>
+#include<numeric>
+#include<climits>
+
+#define ll long long
+#define MOD 1000000007
+#define _z ios_base ::sync_with_stdio(false);cin.tie(nullptr)
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+//#define for(z,x,y) for(int i=z;i<x;i+=y)
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+#define endl "\n"
+#define int long long int
+
+int ans,temp,sum;
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    bool exist=0;
+    if(n&(n-1))
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+    
+}
+
+int32_t main()
+{
+    _z;
+    int t=1;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
