f881f8256abd234e1c459ceb42f47e57092d50a9
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:23:24 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1293A/153615392.cpp
@@ -0,0 +1,77 @@
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//Author:I_Am_A_Noob
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#include<bits/stdc++.h>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define ll long long
+#define MOD 1000000007
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//#define for(z,x,y) for(int i=z;i<x;i+=y)
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+#define endl "\n"
+#define int long long int
+#define all(x) (x).begin(), (x).end()
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define si set<int>
+#define mpii map<int, int>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+void swap(char &x,char&y) {char temp= x; x=y; y=temp;}
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int ans,temp,sum;
+using namespace std;
+
+void solve(){
+    int n,s,k;
+    cin>>n>>s>>k;
+    vi unavailable;
+    si st;
+    for(int i=0;i<k;i++){
+    	cin>>temp;
+    	unavailable.pb(temp);
+    	st.insert(temp);
+    }
+    if(!st.count(s)){
+    	cout<<"0"<<endl;
+    	return;
+    }
+    int backwards=INT_MIN;
+    int forwards=INT_MAX;
+    for(int i=s-1;i>=s-k&&(i>0);i--){
+    	if(!(st.count(i)))
+    	backwards=max(i,backwards);
+    }
+    for(int i=s+1;i<=s+k&&(i<=n);i++){
+    	if(!st.count(i))
+    	forwards=min(forwards,i);
+    }
+    // cout<<min(forwards-s,s-backwards);
+    if(backwards>0&&(s-backwards)<=(forwards-s))
+    	cout<<s-backwards<<endl;
+    else
+    	cout<<forwards-s<<endl;
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+#endif
+    fastio();
+    int t=1;
+    cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
