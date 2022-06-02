9d384ceca385f2cbcacbbcb50fc97844beaeb45a
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:15:24 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/2A/142025930.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<iterator>
+#include<vector>
+#include<set>
+#include<list>
+#include<stack>
+#include<queue>
+#include<math.h>
+#include<iomanip>
+#include<numeric>
+#include<climits>
+#include<map>
+#define ll long long
+#define MOD 1000000007
+using namespace std;
+int main()
+{
+    long int i,j,n,m,x;
+    cin>>n;
+        string s[10000];
+        int a[10000];
+        map<string,long int>mp;
+        for(i=0;i<n;i++)
+        {
+            cin>>s[i]>>a[i];
+            mp[s[i]]+=a[i];
+        }
+        long int mx=0;
+        for(i=0;i<n;i++)
+        {
+            mx=max(mp[s[i]],mx);
+        }
+        map<string,long int>mp2;
+        string ans;
+        for(i=0;i<n;i++)
+        {
+            mp2[s[i]]+=a[i];
+            if(mp2[s[i]]>=mx&&mp[s[i]]==mx)
+            {
+                ans=s[i];
+                break;
+            }
+        }
+        cout<<ans;
+    return 0;
+}
