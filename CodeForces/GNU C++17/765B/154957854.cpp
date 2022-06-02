580c5d1a9dc2061819387e1561774aea381f2e21
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:06:49 +0530

    Committing files

---
+++ CodeForces/GNU C++17/765B/154957854.cpp
@@ -0,0 +1,87 @@
+/*---------------------------------------------------------------------------------------------------------------------------*/
+// Author : I_Am_A_Noob
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#include<bits/stdc++.h>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//constants and typedefs
+#define int long long int
+#define MOD 10000000017
+#define epsilon = 0.0000001f
+#define PI 3.141592653589793238462
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//operations on containers
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//general operations
+#define endl "\n"
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+/*---------------------------------------------------------------------------------------------------------------------------*/
+//templates
+//template <class T> void _print(vector <T> v);
+//template <class T> void _print(set <T> v);
+/*---------------------------------------------------------------------------------------------------------------------------*/
+#define vi vector<int>
+#define vpii vector<pair<int, int>>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define si set<int>
+#define mpii map<int, int>
+/*---------------------------------------------------------------------------------------------------------------------------*/
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+void swap(char &x,char&y) {char temp= x; x=y; y=temp;}
+// ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int ans,temp,sum;
+using namespace std;
+void _print(long long t) {cout << t;}
+template <class T> void _print(vector <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]";}
+void solve(){
+    string s;
+    cin>>s;
+    vi v(26,-1);
+    si st;
+    int prev=-1;
+    bool ans=1;
+    for(int i=0;i<s.length();i++){
+    	if(s[i]=='a'){
+    		st.insert(97);
+    		continue;
+    	}
+    	if(!st.count(s[i]-1)){
+    		// cout<<i<<endl;
+    		// cout<<s[i]-1<<endl;
+    		ans=0;
+    	}
+    	else
+    		st.insert(s[i]);
+    }
+    // for(auto it:st)
+    // 	cout<<it<<" ";
+    // cout<<endl;
+    if(ans)
+    	cout<<"YES"<<endl;
+    else
+    	cout<<"NO"<<endl;
+// cout<<endl;	
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+#endif
+    fastio();
+    int t=1;
+    // cin>>t;
+    while(t--){
+    solve();
+    }
+return 0;
+}
