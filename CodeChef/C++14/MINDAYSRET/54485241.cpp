f70953df2a76d7ef78b614686bd56ffcbb1f095a
vishalaitha <76663093+vishalaitha@users.noreply.github.com>
2022-06-02 15:05:23 +0530

    Committing files

---
+++ CodeChef/C++14/MINDAYSRET/54485241.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	int n,k;
+	cin>>n>>k;
+	cout<<ceil(float(n)/k)<<endl;
+	}
+	return 0;
+}
